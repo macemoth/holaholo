3f7786ac30b3483ce9b01d43cf9c79076fbabd90
Marc Soler <m.e.soler@me.com>
2022-11-11 18:25:07 +0100

    Add request print

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ server = Flask(__name__)
 @server.route('/classify', methods=["POST"])
 def classify_request():
     data = request.get_data(as_text=True)
+    print(data)
     df = pd.read_csv(StringIO(data))
     c = classifier.classify(df)
     if len(c) > 0:
