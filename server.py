82f84012e9f24ae8374f4f5cfbf3f6e5b512d9aa
Marc Soler <m.e.soler@me.com>
2022-11-23 20:10:59 +0100

    Add datetime export

--- server.py
+++ server.py
@@ -12,7 +12,9 @@ def classify_request():
     print(data)
     df = pd.read_csv(StringIO(data))
     c = classifier.classify(df)
+    df.iloc[-1,]
+    date = str(pd.to_datetime(df["eyeDataTimestamp"].iloc[-1], unit="ms"))
     if len(c) > 0:
-        return[c[0]]
+        return jsonify([[c[0]][0], date])
     else:
         return "Unknown"
